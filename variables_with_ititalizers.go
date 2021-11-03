469bf222aa56b4ed968967d5445e6029a02ffff7
Stephanie Moore <68564461+the-coding-forester@users.noreply.github.com>
2021-11-03 19:12:00 -0400

    Comment to explain short variable declarations

--- variables_with_ititalizers.go
+++ variables_with_ititalizers.go
@@ -5,7 +5,9 @@ import "fmt"
 var i, j int = 1, 2
 
 func main() {
+	//short variation declarations can be made inside of functions using := to replace var at the beginning
 	var c, python, java = true, false, "no!"
+	// c, python, java := true, false, "no!"
 	fmt.Println(i, j, c, python, java)
 }
 
